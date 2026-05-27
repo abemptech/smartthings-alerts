803489ecfb913496e77735519b8be9a736051a27
abemptech <abemptech@gmail.com>
2026-05-27 15:48:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const APPS = {
   '1': { clientId: process.env.ST_CLIENT_ID, clientSecret: process.env.ST_CLIENT_SECRET },
   '2': { clientId: process.env.ST_CLIENT_ID_2, clientSecret: process.env.ST_CLIENT_SECRET_2 },
   '3': { clientId: process.env.ST_CLIENT_ID_3, clientSecret: process.env.ST_CLIENT_SECRET_3 },
-  '4': { clientId: process.env.ST_CLIENT_ID_4, clientSecret: process.env.ST_CLIENT_SECRET_4 }.
+  '4': { clientId: process.env.ST_CLIENT_ID_4, clientSecret: process.env.ST_CLIENT_SECRET_4 },
  'GV': { clientId: process.env.ST_CLIENT_ID_GV, clientSecret: process.env.ST_CLIENT_SECRET_GV }
 };
 
@@ -79,7 +79,6 @@ app.get('/update-app/:appId/:appNum', async (req, res) => {
   } catch (err) {
     res.json({ error: err.message, details: err.response?.data });
   }
-});
 });
 
 // Check app details
